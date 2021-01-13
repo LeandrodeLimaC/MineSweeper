b307204ff7e726fd004e7ec5175aaad162f65c61
leanlimac <leandrolima7472@gmail.com>
2021-01-12 21:17:20 -0300

    Basic form of table

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,43 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <table>
+      <td v-for="(row, index) in grid" :key="index">
+        <tr v-for="(column, index) in row" :key="index" v-on:click="test">
+          {{
+            index
+          }}
+        </tr>
+      </td>
+    </table>
   </div>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
-
 export default {
-  name: "App",
-  components: {
-    HelloWorld
-  }
+  data: () => {
+    return {
+      totalRows: 10,
+      totalColumns: 10,
+    };
+  },
+  computed: {
+    grid: function () {
+      let array = new Array(this.totalRows);
+
+      for (let i = 0; i < array.length; i++) {
+        array[i] = new Array(this.totalColumns);
+      }
+
+      return array;
+    },
+  },
+  methods: {
+    test: () => {
+      console.log("olá");
+    },
+  },
 };
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
 </style>
