81bb581f996ee847507b929e5db218db12fa9352
leanlimac <leandrolima7472@gmail.com>
2021-01-13 21:50:53 -0300

    Added floodfill strat

--- src/App.vue
+++ src/App.vue
@@ -7,6 +7,7 @@
           v-for="(row_item, row_index) in column_item"
           :key="row_index"
         >
+          <!-- {{ row_item }} -->
           <cell
             :column_index="column_index"
             :row_index="row_index"
@@ -31,6 +32,7 @@ export default {
       totalRows: 10,
       totalColumns: 10,
       grid: [],
+      totalMines: 10,
     };
   },
   created() {
@@ -39,10 +41,14 @@ export default {
   },
   methods: {
     makeGrid() {
-      let array = new Array(this.totalRows);
+      let array = new Array(this.totalColumns);
 
       for (let i = 0; i < array.length; i++) {
-        array[i] = new Array(this.totalColumns);
+        let a = Array(this.totalRows),
+          j = 0;
+
+        while (j < this.totalRows) a[j++] = { isRevealed: false };
+        array[i] = a;
       }
 
       return array;
@@ -50,10 +56,16 @@ export default {
     setMines() {
       this.grid.map((column, column_index) => {
         for (let j = 0; j < column.length; j++) {
-          this.$set(this.grid[column_index], j, {
-            isMine: Boolean(Math.round(Math.random())),
-            isRevealed: false,
-          });
+          let boolean;
+
+          if (Boolean(Math.round(Math.random())) && this.totalMines) {
+            boolean = true;
+            this.totalMines--;
+          } else {
+            boolean = false;
+          }
+
+          this.$set(this.grid[column_index][j], "isMine", boolean);
         }
       });
     },
@@ -75,16 +87,40 @@ export default {
 
       return nearMines;
     },
+
     handleCellEvent(event) {
       const { column_index, row_index } = event;
+      let cell = this.grid[column_index][row_index];
 
       this.$set(
-        this.grid[column_index][row_index],
+        cell,
         "nearMines",
         this.countNearMines(column_index, row_index)
       );
+      this.$set(cell, "isRevealed", true);
 
-      this.$set(this.grid[column_index][row_index], "isRevealed", true);
+      if (!cell.isMine && !cell.nearMines) {
+        this.floodFill(column_index, row_index);
+      }
+    },
+
+    floodFill(column_index, row_index) {
+      for (let xoff = -1; xoff <= 1; xoff++) {
+        for (let yoff = -1; yoff <= 1; yoff++) {
+          let i = column_index + yoff;
+          let j = row_index + xoff;
+
+          if (i > -1 && i < this.totalColumns && j > -1 && j < this.totalRows) {
+            let neighbor = this.grid[i][j];
+
+            if (!neighbor.isMine && !neighbor.isRevealed) {
+              this.$set(neighbor, "isRevealed", true);
+
+              this.handleCellEvent({ column_index: i, row_index: j });
+            }
+          }
+        }
+      }
     },
   },
 };
